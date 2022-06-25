ff1889c128082f252600bc086db7e111ac44a8aa
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2022-06-25 21:29:09 +0530

    Program: Addition

---
+++ ICSE Class 9/Programs/2. Addition/Addition.java
@@ -0,0 +1,11 @@
+public class Addition
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        String str = "Addition of "+a+" and "+b+" is "+c;
+        System.out.println(str);
+    }
+}
