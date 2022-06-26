6da9e25c2627393b683f9b19463b858a36eae1b2
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2022-06-26 09:52:20 +0530

    Pattern 6

---
+++ ICSE Class 9/Programs/3. Patterns/Pattern6.java
@@ -0,0 +1,16 @@
+public class Pattern6
+{
+    public static void main(String[] args)
+    {
+        int i, j;
+        
+        for(i = 5; i >= 1; i--)
+        {
+            for(j = 5; j >= i; j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
