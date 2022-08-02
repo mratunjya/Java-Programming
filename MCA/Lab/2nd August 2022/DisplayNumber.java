9038860f85184d89d73c60c05dfc008a5e34b857
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2022-08-02 18:29:13 +0530

    2nd August 2022

---
+++ MCA/Lab/2nd August 2022/DisplayNumber.java
@@ -0,0 +1,11 @@
+public class DisplayNumber
+{
+    public static void main(String args[])
+    {
+        System.out.println("Numbers between 1 and 10:");
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
