fb0e0a0eb85aab9b276bc13dca7c2e6817eb0f12
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2022-08-30 16:05:34 +0530

    26th July 2022\Addition.java cleanup

--- MCA/Lab/26th July 2022/Addition.java
+++ MCA/Lab/26th July 2022/Addition.java
@@ -3,7 +3,6 @@ public class Addition
     public static void main(String args[])
     {
         int a = 10, b = 15;
-.
-        System.out.println("Sum of "+a+" and "+b+" is "+(a+b));
+        System.out.println("Addition of " + a + " and " + b + " is " + (a + b));
     }
 }
