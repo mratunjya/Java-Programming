13de7646cb321e6f75925fa4246478c6bc119428
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2022-11-26 15:29:40 +0530

    10. Patterns (Part I)

---
+++ 10. Patterns (Part I)/InvertedStarPattern.java
@@ -0,0 +1,16 @@
+public class InvertedStarPattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = i; j < 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output:
+// * * * *
+// * * *
+// * *
+// *
